502caee7051f446bb098ea17691a37e7aa8c4692
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-16 20:42:10 -0600

    put in the js option

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i]
+  }
+  return newString;
 }
 
 if (require.main === module) {
@@ -11,9 +15,31 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+  
+  console.log("Expecting: dlrow olleH")
+  console.log("=>", reverseString("Hello world"))
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+// **create a function that will take a string and reverse it
+//  function reverseString(str) {
+  // **create an empty string
+  // let newString = "";
+  // **create a for loop that will go through the string
+  // for (let i = str.length - 1; i >= 0; i--) {
+    // **add the current letter to the empty string
+  //   newString += str[i];
+  // }
+
+  // **create a test that will test the string
+  // if (newString === "ih") {
+  //   console.log("hi");
+  // }
+  // if (newString === "ybabtac") {
+  //   console.log("catbaby");
+  // }
